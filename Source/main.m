c6932c18e4a69fc39467dd4cc9c0399ba41e14b5
Patrick B. Gibson <patrick@fadeover.org>
2011-01-12 16:41:59 -0800

    Initial Commit

---
+++ Source/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  gaysailor
+//
+//  Created by Patrick B. Gibson on 11-01-12.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    // insert code here...
+    NSLog(@"Hello, World!");
+    [pool drain];
+    return 0;
+}
+
